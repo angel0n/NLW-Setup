26780fdab768b5ea79e457994453b32eb4a96773
angel0n <63112226+angel0n@users.noreply.github.com>
2023-01-18 17:34:20 -0300

    aula 2 01:26:08

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,13 @@
+import { Header } from "./components/Headers"
+import { SummaryTable } from "./components/SummaryTable"
 import "./styles/global.css"
 
-import logoImage from "./assets/Logo.svg"
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-        <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
-          <img src={logoImage} />
-          <button
-            type="button"
-            className="border border-violet-500 font-semibold rounded px-6 py-4"
-          >
-            Nono Hábito
-          </button>
-        </div>
+        <Header />
+        <SummaryTable />
       </div>
     </div>
   )
