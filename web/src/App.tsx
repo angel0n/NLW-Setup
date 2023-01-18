2c0151ed487afc0784220dd2e550c68bf6799954
angel0n <63112226+angel0n@users.noreply.github.com>
2023-01-18 17:08:41 -0300

    aula 2 01:09:44

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,21 @@
-import { Habit } from "./components/Habit"
 import "./styles/global.css"
 
-function App() {
+import logoImage from "./assets/Logo.svg"
+export function App() {
   return (
-    <>
-      <Habit completed={3} />
-      <Habit completed={5} />
-      <Habit completed={7} />
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
+          <img src={logoImage} />
+          <button
+            type="button"
+            className="border border-violet-500 font-semibold rounded px-6 py-4"
+          >
+            Nono Hábito
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default App
