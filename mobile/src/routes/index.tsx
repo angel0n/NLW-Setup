4d494066488efd3853e931cc143b5e9f6e2af7b7
angel0n <63112226+angel0n@users.noreply.github.com>
2023-01-19 21:44:43 -0300

    Aula 03 01:47:42

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { AppRoutes } from './app.routes'
 export function Routes(){
     return(
-        <View className='flex bg-background'>
+        <View className='flex-1 bg-background'>
             <NavigationContainer>
                 <AppRoutes />
             </NavigationContainer>
--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,9 +1,21 @@
-import { View } from "react-native";
+import { View, ScrollView,Text } from "react-native";
+import { BackButton } from "../components/BackButton";
 
 export function New(){
     return(
         <View className="flex-1 bg-background px-8 pt-16">
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <BackButton />
 
+                <Text className="mt-6 text-white font-extrabold text-3xl">
+                    Criar hábito
+                </Text>
+
+                
+                <Text className="mt-6 text-white font-semibold text-base">
+                    Qual seu comprometimento?
+                </Text>
+            </ScrollView>
         </View>
     )
 }
